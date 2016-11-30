556a4eb6e787c0e62481f601092bc98a25e1508b
Administrator <wdjxysc@163.com>
2016-11-30 18:25:46 +0800

    html5 定位拖拽

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,14 @@
 var myImage = document.querySelector('img');
 
-if(myImage != null)
-myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/shu.png') {
-      myImage.setAttribute ('src','images/t.png');
-    } else {
-      myImage.setAttribute ('src','images/shu.png');
+if (myImage != null)
+    myImage.onclick = function() {
+        var mySrc = myImage.getAttribute('src');
+        if (mySrc === 'images/shu.png') {
+            myImage.setAttribute('src', 'images/t.png');
+        } else {
+            myImage.setAttribute('src', 'images/shu.png');
+        }
     }
-}
 
 
 var myButton = document.querySelector('button');
@@ -18,26 +18,26 @@ var myHeading = document.querySelector('h1');
 
 
 function setUserName() {
-  var myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName);
-  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
 }
 
 
-if(!localStorage.getItem('name')) {
-  setUserName();
+if (!localStorage.getItem('name')) {
+    setUserName();
 } else {
-  var storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+    var storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
 }
 
 myButton.onclick = function() {
-  // setUserName();
-  goToMyPage();
+    // setUserName();
+    goToMyPage();
 }
 
-function goToMyPage(){
-  var path = window.location.href;
-  path = path.substr(0, path.lastIndexOf('/') + 1)
-  window.location.href =  path + "mypage.html";
-} 
+function goToMyPage() {
+    var path = window.location.href;
+    path = path.substr(0, path.lastIndexOf('/') + 1)
+    window.location.href = path + "mypage.html";
+}
