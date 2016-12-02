7fece8eb7a69a6966af6505a7e53b961a6420c22
Administrator <wdjxysc@163.com>
2016-12-02 11:14:38 +0800

    20161202

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+var myImage = document.querySelector('img');
+
+if (myImage != null)
+    myImage.onclick = function() {
+        var mySrc = myImage.getAttribute('src');
+        if (mySrc === 'images/shu.png') {
+            myImage.setAttribute('src', 'images/t.png');
+        } else {
+            myImage.setAttribute('src', 'images/shu.png');
+        }
+    }
+
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    var ssss = "sfsadf";
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+}
+
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+    // setUserName();
+    goToMyPage();
+}
+
+function goToMyPage() {
+    var path = window.location.href;
+    path = path.substr(0, path.lastIndexOf('/') + 1)
+    window.location.href = path + "mypage.html";
+}
