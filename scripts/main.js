dfa9994f7107c449a95dccad30063deeb10f8e41
Administrator <wdjxysc@163.com>
2016-12-03 09:49:10 +0800

    合并提交

--- scripts/main.js
+++ scripts/main.js
@@ -14,17 +14,8 @@ if (myImage != null)
 var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
 
-
-
-<<<<<<< HEAD
 function setUserName() {
     var myName = prompt('Please enter your name.');
-    var ssss = "sfsadf";
-=======
-
-function setUserName() {
-    var myName = prompt('Please enter your name.');
->>>>>>> 1170afb930e97a703a221f0bf0a6da17706b90ae
     localStorage.setItem('name', myName);
     myHeading.innerHTML = 'Mozilla is cool, ' + myName;
 }
